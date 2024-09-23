b2454c8e031c3f2083e3d13d14ef72f42b76b39d
francoAgustin07 <faluque@udc.edu.ar>
2024-09-23 10:17:52 -0300

    Avanzo un poco con las views, hago les de crear la publicacion y la de listarlas todas y en detalle a una sola. Importo todo lo necesario ahora y para mas adelante

--- post/views.py
+++ post/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView, DetailView
+from post.models import Publicacion
+from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home.html')
+
+class CrearPublicacion(LoginRequiredMixin, CreateView):
+    model = Publicacion
+    fields = ['titulo','contenido']
+    template_name = 'crear_publicacion.html'
+    success_url = reverse_lazy('home') 
+    
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+    
+class ListarPublicaciones(LoginRequiredMixin, ListView):
+    model = Publicacion
+    template_name = 'listar_publicaciones.html'
+    context_object_name = 'publicaciones'
+    
+    def get_queryset(self):
+        return Publicacion.objects.all()
+    
+class ListarEnDetalle(LoginRequiredMixin, DetailView):
+    model = Publicacion
+    template_name = 'publicacion_en_detalle.html'
+    context_object_name = 'publicacion'
